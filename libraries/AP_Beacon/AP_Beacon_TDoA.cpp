054aab04b4426a25721ea298da6eb0a2067dd573
IDaniele <IDaniele@users.noreply.github.com>
2017-11-09 12:11:25 +0100

    Add files via upload

---
+++ libraries/AP_Beacon/AP_Beacon_TDoA.cpp
@@ -0,0 +1,28 @@
+#include <AP_HAL/AP_HAL.h>
+#include "AP_Beacon_TDoA.h"
+#include <ctype.h>
+#include <stdio.h>
+
+extern const AP_HAL::HAL& hal;
+
+// constructor
+AP_Beacon_TDoA::AP_Beacon_TDoA(AP_Beacon &frontend, AP_SerialManager &serial_manager) :
+    AP_Beacon_Backend(frontend)
+{
+    if(this->_tdoa == nullptr){
+        this->_tdoa = new AP_TDoA;
+        this->_tdoa->init();
+        this->_tdoa->conf_dwm();
+    }
+    /*_tdoa->setAnchorPosition(&_frontend.beacon1_pos, &_frontend.beacon2_pos, &_frontend.beacon3_pos,
+                             &_frontend.beacon4_pos, &_frontend.beacon5_pos, &_frontend.beacon6_pos, &_frontend.beacon7_pos);
+    */
+}
+
+// update the state of the sensor
+void AP_Beacon_TDoA::update(void)
+{
+    _tdoa->enqueue();
+    float nonlosoancora = 1.0f;
+    set_vehicle_position(_tdoa->getEstimatedPosition(), nonlosoancora);
+}
